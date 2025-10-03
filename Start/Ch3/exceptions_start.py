00f8b4677d668fd62022b4fc2d0d781a59b79f01
vayiagiannakop <vayiagiannakop@gmail.com>
2025-10-03 18:41:47 +0000

    My updates to the project

--- Start/Ch3/exceptions_start.py
+++ Start/Ch3/exceptions_start.py
@@ -7,7 +7,41 @@
 
 # Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
+# try:
+#   x=10/0
+# except:
+#   print("You can't divide by zero!")
+# 
+# Python code​​​​​​‌‌‌​​‌​‌​​‌‌‌‌​​‌​‌‌‌​​​‌ below
+# Use print("messages...") to debug your solution.
 
+def is_palindrome(teststr):
+    # Your code goes here.
+    teststr = teststr.strip()
+    teststr = teststr.upper()
+    teststr = teststr.replace(" ", "").replace("!", "").replace(",", "")
+    teststr = teststr.replace(".", "")
+    teststr = teststr.replace("?", "").replace(";", "").replace(":", "")
+    teststr = teststr.replace("-", "") 
+    teststr = teststr.replace("_", "").replace("@", "")
+    teststr = teststr.replace("#", "").replace("$", "")
+    teststr = teststr.replace("'", "").replace("^", "")
+    teststr = teststr.replace("&", "")    
+    teststr = teststr.replace(" ", "").replace("!", "")
+    teststr = teststr.replace(",", "").replace(".", "")
+    if teststr == teststr[::-1]:
+      return True
+    else:
+      return False
+    if teststr == teststr[::-1]:
+      return True
+    else:
+       return False
+    
+print(is_palindrome("Hello !! "))
+print(is_palindrome("A   !!??d  a"))
+
+  
 
 # You can also catch specific exceptions
 
--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -2,10 +2,23 @@
 # Working with modules of code
 
 # import the math module, which contains features for working with mathematics
+# import math
+# print(f"the square root of 16 is {math.sqrt(16)}")
+# print(f"the value of pi is {math.pi}")
+# print(f"the pow(2,10) is {math.pow(2,10)}")
 
 
 # import a specific part of the module so you can refer to it more easily
-
+# from math import pi
+# print(f"the value of pi is {pi}")
+# ss=[]
+# import random as r
+# for i in range(10):
+#    print(f"A random number between 1 and 10 is {r.randint(1,10)}")
+#    ss.append(r.randint(1,10))
+# print(ss)
+# print(f"The sum of the list is {sum(ss)}") 
+   
 
 # import a module and give it a different name
 
@@ -30,5 +43,9 @@ data = [
   ["Mouse", 24.99, 128],
   ["Keyboard", 59.99, 89]
 ]
+# https://pypi.org/project/tabulate/
+from tabulate import tabulate
+print(tabulate(data, headers="firstrow", tablefmt="double_grid"))    
+
 
 # Create a formatted table
